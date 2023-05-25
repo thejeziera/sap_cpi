8674bf3ac920b42b0f57a9c6bdd9e4f07183de5d
thejeziera <jezierskimichal1@gmail.com>
2023-05-25 13:52:51 +0200

    X-Message-ID script - much cleaner version from a friend

--- src/SAP_CPI_Scripts/Monitoring_and_Alerting/setX_Message_ID.groovy
+++ src/SAP_CPI_Scripts/Monitoring_and_Alerting/setX_Message_ID.groovy
@@ -1,29 +1,16 @@
-import com.sap.gateway.ip.core.customdev.util.Message;
-import java.util.HashMap;
-import org.w3c.dom.Node;
-import groovy.xml.*
+import com.sap.gateway.ip.core.customdev.util.Message
 
 def Message processData(Message message) {
+    def headers = message.getHeaders()
+    def messageLog = messageLogFactory.getMessageLog(message)
+    def X_Message_ID = headers["X-Message-ID"]
 
-    def map = message.getHeaders();
-    def messageLog = messageLogFactory.getMessageLog(message);
-
-//fetch existing X-Message-ID
-    def X_Message_ID = map.get("X-Message-ID");
-
-//IF X-MESSAGE-ID already exist catch it for monitoring ELSE set CPI ID as new X-MESSAGE-ID for monitoring and beyond
-    if(messageLog != null){
-
-        if(X_Message_ID != null){
-
-            messageLog.addCustomHeaderProperty("X-Message-ID", X_Message_ID);
-        }
-        else {
-            def CPI_ID = map.get("SAP_MessageProcessingLogID");
-            messageLog.addCustomHeaderProperty("X-Message-ID", CPI_ID);
-            message.setHeader("X-Message-ID", CPI_ID);
-        }
+    if (messageLog && X_Message_ID == null) {
+        X_Message_ID = headers["SAP_MessageProcessingLogID"]
+        message.setHeader("X-Message-ID", X_Message_ID)
     }
 
-    return message;
+    messageLog?.addCustomHeaderProperty("X-Message-ID", X_Message_ID)
+
+    return message
 }
